62527c6ff39ecfce75ec2861d94f975800520c00
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-06-23 14:14:21 +0430

    Add some widget to second page

--- lib/screen/second.dart
+++ lib/screen/second.dart
@@ -11,6 +11,20 @@ class Second extends StatelessWidget {
         label: Text('Back'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.purple,
+          ),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.green,
+          )
+        ],
+      ),
     );
   }
 }
